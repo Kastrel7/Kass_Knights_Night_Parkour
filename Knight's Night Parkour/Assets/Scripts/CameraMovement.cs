36d091dae534012fe05dbf96905aee706d9dc3a8
Kastrel7 <jazzjunior1@gmail.com>
2024-02-23 11:46:16 +0000

    Signed-off-by: Kastrel7 <jazzjunior1@gmail.com>

--- Knight's Night Parkour/Assets/Scripts/CameraMovement.cs
+++ Knight's Night Parkour/Assets/Scripts/CameraMovement.cs
@@ -7,8 +7,8 @@ public class CameraMovement : MonoBehaviour
     public float sensitivity = 2;
     public Transform target;
     public float disFromTarget = 5;
+    Vector2 yawMinMax = new Vector2 (-90, 90);
 
-    public float rotationSmoothTime = 0.12f;
     Vector3 rotationSmoothVelocity;
     Vector3 currentRotation;
 
@@ -20,9 +20,23 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+        float rotationSmoothTime = 0.12f;
         yaw += Input.GetAxis("Mouse X") * sensitivity;
+        //yaw = yaw % 720;
+        yaw = Mathf.Repeat(yaw, 360f);
 
-        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(height, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    yaw = Mathf.Clamp(yaw, target.eulerAngles.y - 85, target.eulerAngles.y + 85);
+        //}
+
+
+        //if ( Mathf.Abs(transform.eulerAngles.y - target.eulerAngles.y) >= 90 )
+        //{
+        //    yaw = Mathf.Clamp(yaw, -90, 90);
+        //}
+
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(height, yaw), ref rotationSmoothVelocity, 0);
 
         transform.eulerAngles = currentRotation;
 
