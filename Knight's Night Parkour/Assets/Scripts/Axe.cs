7ce8a738c27513abc333fc6a62e3114bd0b889d8
Kastrel7 <jazzjunior1@gmail.com>
2024-04-25 02:44:33 +0100

    Signed-off-by: Kastrel7 <jazzjunior1@gmail.com>

--- Knight's Night Parkour/Assets/Scripts/Axe.cs
+++ Knight's Night Parkour/Assets/Scripts/Axe.cs
@@ -4,20 +4,31 @@ using UnityEngine;
 
 public class Axe : MonoBehaviour, IInteractable
 {
+    Rigidbody rb;
+    float starty;
+    Vector3 start;
+
     public void Interact(Collider other)
     {
         other.GetComponent<Transform>().position = Character.respawn;
     }
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        starty = transform.position.y; 
+        start = transform.position;
+    }
+
     private void Update()
     {
-        if (transform.position.y < 14)
+        if (transform.position.y < starty - 7)
         {
-            this.GetComponent<Rigidbody>().AddForce(new Vector3(0, 100, 0));
+            rb.AddForce(new Vector3(0, 100, 0));
         }
-        else if (transform.position.y > 21)
+        else if (transform.position.y > starty)
         {
-            transform.position = new Vector3(0, 20.85f, 234);
+            transform.position = start;
         }
     }
 }
--- Knight's Night Parkour/Assets/Scripts/Floor.cs
+++ Knight's Night Parkour/Assets/Scripts/Floor.cs
@@ -1,17 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Floor : MonoBehaviour, IInteractable
 {
     public Rigidbody axe;
+
     public void Interact(Collider other)
     {
-        axe.AddForce(new Vector3(0, -50000, 0));
-    }
-
-    private void Update()
-    {
-        
+        axe.AddForce(new Vector3(0, -1000, 0), ForceMode.Impulse);
     }
 }
